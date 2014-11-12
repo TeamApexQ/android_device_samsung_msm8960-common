cc0e30c075c17e2d80d716621ffcdb3ceb8deb9f
lbdroid <lbdroid@users.noreply.github.com>
2014-11-12 16:05:57 -0500

    Yes, we have no gps for now

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -83,7 +83,7 @@ TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
 # KERNEL_HAS_GETTIMEOFDAY_HELPER := true
 
 # We have the new GPS driver
-BOARD_HAVE_NEW_QC_GPS := true
+#BOARD_HAVE_NEW_QC_GPS := true
 
 # Use CAF media driver variant for 8960
 # TARGET_QCOM_MEDIA_VARIANT := caf
